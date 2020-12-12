e53472cf5d03a24423c330a1aba8d2cd5adbbeb5
Yuri Cavalcanti <yuri.cavalcanti2013@gmail.com>
2020-12-12 16:55:40 -0300

    Aula96 Done

--- Quizzler-iOS/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "Four + Two is equal to Six.",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+    
     }
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if questionNumber < quiz.count - 1{
+            questionNumber += 1
+            updateUI()
+        } else {
+            print ("Ja deu - Index out of range")
+        }
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
 
 }
 
