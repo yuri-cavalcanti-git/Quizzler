48923a82287a502f88a429d303d40ac6733c3895
Yuri Cavalcanti <yuri.cavalcanti2013@gmail.com>
2020-12-12 17:33:03 -0300

    Aula 97 Done

--- Quizzler-iOS/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS/Quizzler-iOS13/ViewController.swift
@@ -15,10 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    let quiz = [
-        "Four + Two is equal to Six.",
-        "Five - Three is greater than One",
-        "Three + Eight is less than Ten"
+    let arrayQuiz = [
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
     ]
     var questionNumber = 0
     
@@ -28,16 +28,27 @@ class ViewController: UIViewController {
     
     }
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        if questionNumber < quiz.count - 1{
-            questionNumber += 1
-            updateUI()
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = arrayQuiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
         } else {
-            print ("Ja deu - Index out of range")
+            print("Wrong!")
         }
+        
+        if questionNumber < arrayQuiz.count - 1 {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        updateUI()
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = arrayQuiz[questionNumber][0]
     }
 
 }
